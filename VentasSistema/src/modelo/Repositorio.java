bd31038a1083a039320f06ce5323f47ce9c55b14
Federico Corbalán <fedecorbalan10@gmail.com>
2025-11-27 18:56:41 -0300

    update interface

--- VentasSistema/src/modelo/Repositorio.java
+++ VentasSistema/src/modelo/Repositorio.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author fede
- */
-public interface Repositorio {
-    
+import java.util.List;
+
+public interface Repositorio<T> {
+    List<T> cargar();
+    void guardar(List<T> lista);
 }
