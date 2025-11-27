b2bb5baad3abbefb1e54a1a65ca01e0c743ca4e3
Federico Corbalán <fedecorbalan10@gmail.com>
2025-11-27 19:01:42 -0300

    Update RepositorioProductos.java
    se implementan las funciones serializadoras

--- VentasSistema/src/modelo/RepositorioProductos.java
+++ VentasSistema/src/modelo/RepositorioProductos.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author fede
- */
-public class RepositorioProductos {
+import java.io.*;
+import java.util.*;
+
+public class RepositorioProductos implements Repositorio<Producto> {
     
+    private final String archivo = "productos.dat";
+    
+    @Override
+    public List<Producto> cargar() {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(archivo))) {
+            return (List<Producto>) in.readObject();
+        } catch (Exception e) {
+            System.out.println("Error al leer archivo: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+    
+    @Override
+    public void guardar(List<Producto> lista) {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(archivo))) {
+            out.writeObject(lista);
+        } catch (Exception e) {
+            System.out.println("Error al guardar archivo: " + e.getMessage());
+        }
+    }
 }
